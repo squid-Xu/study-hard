59ca7c57292b2b70ff6bf114a9a31c497ccefdf4
squid-Xu <xmq0619@126.com>
2024-03-27 22:09:18 +0800

    vue3组件通信

---
+++ vue3-demo/src/views/props/components/child.vue
@@ -0,0 +1,49 @@
+<template>
+  <p>第一种接收值：{{ first }}</p>
+  <p>第二种接收值:{{ second }}</p>
+  <button @click="$emit('handleSecond', 20)">第二种</button>
+  <p>第三种接收值:{{ third }}</p>
+  <p>第四种接收值:{{ $attrs.four }}</p>
+  <p>第五种接收值:{{ $attrs.fifth }}</p>
+  <p>第六种接收值:{{ sixth }}</p>
+  <button @click="handleTitle">{{ title }}</button>
+  <p>第七种接收值:{{ seventh }}----{{ seventh1 }}</p>
+</template>
+<script setup lang="ts">
+import { inject, ref } from 'vue'
+
+defineProps({
+  first: {
+    type: Number,
+    defaut: 0
+  },
+  second: {
+    type: Number,
+    defaut: 0
+  },
+  third: {
+    type: Number,
+    defaut: 0
+  }
+})
+
+const emit = defineEmits(['handleSecond', 'handleThird'])
+
+function handleThird() {
+  console.log('1111')
+  emit('handleThird')
+}
+
+defineExpose({ handleThird })
+
+const sixth = defineModel()
+
+const title = defineModel('title')
+
+function handleTitle() {
+  title.value = '你好呀'
+}
+
+const seventh = inject('seventh')
+const seventh1 = inject('seventh1')
+</script>
--- vue3-demo/src/views/props/index.vue
+++ vue3-demo/src/views/props/index.vue
@@ -1,3 +1,50 @@
 <template>
-  <h2>组件通信</h2>
+  <h1>vue2组件通信（8种）</h1>
+  <child
+    :first="first"
+    :second="second"
+    @handleSecond="handleSecond"
+    ref="childDom"
+    :third="third"
+    @handleThird="handleThird"
+    :four="four"
+    :fifth="fifth"
+    v-model="sixth"
+    v-model:title="title"
+  ></child>
 </template>
+<script setup lang="ts">
+import { onMounted, provide, ref } from 'vue'
+import child from './components/child.vue'
+
+const first = ref(1)
+const second = ref(2)
+const third = ref(3)
+const four = ref(4)
+const fifth = ref(5)
+const sixth = ref(6)
+const seventh = ref(7)
+const seventh1 = ref(77)
+
+const title = ref('你好')
+
+provide('seventh', seventh)
+provide('seventh1', seventh1)
+
+function handleSecond(val: number) {
+  second.value = val
+}
+
+function handleThird() {
+  third.value = 30
+}
+
+const childDom: any = ref(null)
+
+onMounted(() => {
+  setTimeout(() => {
+    console.log('0000')
+    childDom?.value?.handleThird()
+  }, 1000)
+})
+</script>
